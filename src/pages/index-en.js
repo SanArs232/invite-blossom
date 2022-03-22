4dbe1a136153169d466f3facd55fd730048050ca
Sanja Arsova <sanja.arsova@mway.io>
2022-03-22 14:12:44 +0100

    created intel solution

--- src/pages/index-en.js
+++ src/pages/index-en.js
@@ -2,20 +2,20 @@ import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import ImageRing from "../images/ring-ivanisanja.png"
-import Nav from "../components/header"
+import Nav from "../components/header-en"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
-    <Nav />
     <SEO title="Home" />
+    <Nav />
     <section className="xl:container mx-auto">
       <div className=" text-center bg-wedding py-20">
         <div>
           <h1 className=" font-bold text-zinc-700 md:py-10 ">
-            Добродојдовте на нашата свадебена покана
+            Welcome to our Wedding Invitation
           </h1>
         </div>
         <div className=" ">
@@ -24,8 +24,7 @@ const IndexPage = () => (
 
         <div>
           <h2 className=" px-10 font-bold text-zinc-700 pb-10 md:w-2/4 mx-auto ">
-            Чест и задоволство ни е да ве поканиме на нашата свадбена прослава
-            на ден{" "}
+            We are pleased to invite you to our wedding celebration
           </h2>
           <div className="font-bold text-zinc-700 text-5xl  pb-10">
             {" "}
@@ -34,9 +33,9 @@ const IndexPage = () => (
         </div>
         <div>
           <h2 className=" font-bold text-zinc-700  ">
-            во летниот дел на ресторан „Ваго„
+            in the summer garden of the resorant "Vago"
           </h2>
-          <h2 className=" font-bold text-zinc-700  ">во 19:00 часот</h2>
+          <h2 className=" font-bold text-zinc-700  ">in 19:00 h</h2>
         </div>
       </div>
 
