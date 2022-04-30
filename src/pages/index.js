e736d8fd5f121cd77c0444372357b9f9511c078b
SanArs232 <san.arsova@gmail.com>
2022-04-30 22:33:16 +0200

    update mobile version

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
 import { useState } from "react"
-import ImageRing from "../images/wedding-cover-web.png"
+import ImageRing from "../images/ring-ivanisanja.png"
 import Nav from "../components/header"
 import ImageFlower from "../images/upper-flower.png"
 import ImageFlowerReverse from "../images/upper-flower-reverse.png"
@@ -11,6 +11,11 @@ import IconRings from "../images/icon-rings.png"
 import IconPhoto from "../images/icon-photo.png"
 import IconDance from "../images/icon-dance.png"
 import IconDinner from "../images/icon-dinner.png"
+import FlowerForm from "../images/flower-form.png"
+import FlowerFormReverse from "../images/flowerformreverse.png"
+import RingFlowerCover from "../images/bg-si.png"
+import RingFlowerCoverSm from "../images/wedding-cover-web.png"
+import FooterFlower from "../images/grapich-footer.png"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -55,73 +60,73 @@ const Contact = () => {
 
       <div className="py-10">
       <div className="flex flex-row justify-between">
-        <div>
-            <img src ={ImageFlowerReverse} alt="Logo" className="mx-auto md:-mt-20" />
+        <div className="hidden md:block">
+            <img src ={FlowerForm} alt="Logo" className="mx-auto md:-mt-20" />
           </div>
-
-        <div><h1 className="font-bold text-zinc-700 py-10 md:text-3xl xl:text-5xl ">
-            Добродојдовте на нашата Свадбена Покана
-          </h1>
-        </div>
-
-          <div>
-            <img src ={ImageFlower} alt="Logo" className="mx-auto md:-mt-20" />
-          </div>
-        </div>
-
-        <div className="mx-auto xl:-mt-48">
-              <img src={ImageRing} alt="Logo" className="max-w-auto mx-auto" />
-           </div>
           
-      </div>
-
-        <div div className="font-bold text-zinc-700">
-          <h2 className=" px-10 pb-5 md:w-2/4 mx-auto text-3xl  ">
+        <div>
+        <h2 className=" px-3 md:px-10 pb-5 lg:w-11/12 mx-auto md:text-3xl font-bold text-zinc-700">
             Чест и задоволство ни е да ве поканиме на нашата Свадбена Прослава
             на ден{" "}
           </h2>
-          <div className=" text-5xl pb-5">
-            {" "}
-            24.06.2022
-          </div>
-      
-          <div className="md:text-3xl">
-        <a href="https://goo.gl/maps/wGyno8UJjkjUiwdV8" target="_blank">
-          <h2 className=" md:text-3xl text-zinc-700 py-5 decoration-solid">
-            во летната градина на ресторан <span className="text-zinc-700 font-extrabold">„Ваго„</span>
-          </h2>
-          </a>
-          <span className="text-zinc-700 font-extrabold ">19:30 часот</span>
+          <h1 className="font-bold text-zinc-700 pb-10 lg:pb-20 text-4xl md:text-5xl">
+         26.06.2022   
+          </h1>
+        <div className="h-32 hidden lg:block"></div>
         </div>
 
-        <div className="md:text-xl py-20  text-zinc-700 ">
+          <div className="hidden md:block">
+            <img src ={FlowerFormReverse} alt="Logo" className="mx-auto md:-mt-20" />
+          </div>
+        </div>
+
+        <div className="mx-auto lg:-mt-20 xl:-mt-40">
+              <img src={RingFlowerCover} alt="Logo" className="mx-auto hidden md:block" />
+         </div>
+         <div className="mx-auto lg:-mt-20 xl:-mt-40">
+              <img src={RingFlowerCoverSm} alt="Logo" className="mx-auto block md:hidden" />
+         </div>
+          
+      </div>
+
+        <div div className="font-bold text-zinc-700 lg:mt-40">
+          <div className="md:text-3xl">
+          <a href="https://goo.gl/maps/wGyno8UJjkjUiwdV8" target="_blank" >
+          <h2 className="px-3 md:px-10 md:text-3xl text-zinc-700 pb-5 decoration-solid">
+            Свадбената Прослава ќе се одржи во летната градина на ресторант <span className="text-zinc-700 font-extrabold hover:text-amber-500">„Ваго„</span>
+          </h2>
+          </a>
+          <span className="text-zinc-700 font-extrabold text-2xl md:text-3xl"> во 19:30 часот</span>
+        </div>
+
+        <div className="md:text-xl py-20  text-zinc-700 mx-auto">
         <div className="flex-wrap flex flex-row -mt-10 ">
               <div className="mx-auto flex-2">
-                    <img src={IconCurch} alt="Logo" className="max-w-auto mx-auto h-40 md:h-auto" />
+                    <img src={IconCurch} alt="Logo" className="max-w-auto mx-auto h-28 md:h-auto" />
                     <div className="">
                     16:00 часот
                     </div>
               </div>
               <div className="mx-auto ">
-                    <img src={IconRings} alt="Logo" className="max-w-auto mx-auto h-40 md:h-auto" />
+                    <img src={IconRings} alt="Logo" className="max-w-auto mx-auto h-28 md:h-auto" />
                     <div className="">
                     16:30 часот
                     </div>
               </div>
               <div className="mx-auto ">
-                    <img src={IconPhoto} alt="Logo" className="max-w-auto mx-auto h-40 md:h-auto" />
+                    <img src={IconPhoto} alt="Logo" className="max-w-auto mx-auto h-28 md:h-auto" />
                     <div className="text-lg text-zinc-700">
                     19:30 часот
                     </div>
               </div>
               <div className="mx-auto">
-                    <img src={IconDance} alt="Logo" className="max-w-auto mx-auto h-40 md:h-auto" />
+                    <img src={IconDance} alt="Logo" className="max-w-auto mx-auto h-28 md:h-auto" />
                     <div className="">
                     20:00 часот
                     </div>
               </div>
               <div className="mx-auto">
-                    <img src={IconDinner} alt="Logo" className="max-w-auto mx-auto h-40 md:h-auto" />
+                    <img src={IconDinner} alt="Logo" className="max-w-auto mx-auto h-28 md:h-auto" />
                     <div className="text-lg text-zinc-700">
                     22:00 часот
                     </div>
@@ -131,7 +136,7 @@ const Contact = () => {
         </div>
 
 
-<div className="bg-wedding ">
+  <div className="bg-wedding">
         <form
           onSubmit={handleSubmit}
           name="contact"
@@ -141,22 +146,26 @@ const Contact = () => {
           action="thank-you"
           netlify
         >
-      
-    
-        
-    <div className="flex flex-row justify-between">
-        <div>
-            <img src ={ImageFlowerReverse} alt="Logo" className="mx-auto md:-mt-20" />
-          </div>
-        <div className="pt-20 text-2xl">Ве молиме потврдете го вашето присуство со што ке ја потполните оваа контакна форма</div>
+
           <div>
-            <img src ={ImageFlower} alt="Logo" className="mx-auto md:-mt-20" />
+            <img src ={FooterFlower} alt="Logo" className="mx-auto lg:hidden block " />
+          </div>
+      
+       <div className="flex flex-row justify-between drop-shadow-sm">
+      
+       
+        <div>
+            <img src ={FlowerForm} alt="Logo" className="mx-auto md:-mt-20 hidden lg:block" />
+          </div>
+        <div className="lg:pt-20 pb-5 xl:pt-40 md:text-2xl px-5 md:px-10 ">Ве молиме потврдете го вашето присуство со што ке ја потполните оваа контакна форма</div>
+          <div>
+            <img src ={FlowerFormReverse} alt="Logo" className="mx-auto md:-mt-20 hidden lg:block" />
           </div>
         </div>
 
-        <div className="pb-5 -mt-10 flex-col">
+        <div className="pb-5 flex-col">
         <input type="hidden" name="contact" value="contact" />
-        <span className="px-28 text-2xl text-right" htmlFor="name">Име и Презиме:</span>
+
               <input
                 id="name"
                 type="text"
@@ -164,13 +173,10 @@ const Contact = () => {
                 onChange={handleChange}
                 value={formState.name}
                 placeholder="Внесете име и презиме"
-                className="w-2/4 bg-white border-2 border-amber-500 mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline "
+                className="mx-auto w-11/12 md:w-2/4 bg-white border-2 border-amber-500 mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline "
               />
-           
           </div>
           <div className="py-5 flex-col">
-          
-          <span className="px-28 text-2xl text-right" htmlFor="name">Внесете емаил:</span>
                 <input
                   id="email"
                   type="email"
@@ -178,36 +184,26 @@ const Contact = () => {
                   onChange={handleChange}
                   value={formState.email}
                   placeholder="Внесете го вашиот емаил"
-                  className="w-2/4 bg-white border-2 border-amber-500 mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline "
+                  className="mx-auto w-11/12 md:w-2/4 bg-white border-2 border-amber-500 mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline "
                 />
-             
             </div>
       
           <div className="py-5">
-           <span className="px-5 text-2xl" htmlFor="name">
-              Страна (невеста/младоженец): 
-               </span>
                  <input
                    id="message"
                    type="text"
                    name="message"
                    onChange={handleChange}
                    value={formState.message}
-                   placeholder="невеста/младоженец"
-                   className="w-2/4 bg-white border-2 border-amber-500 mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline"
+                   placeholder="Страна (невеста/младоженец)"
+                   className="mx-auto w-11/12 md:w-2/4 bg-white border-2 border-amber-500 mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline"
                  />
-              
              </div>
 
-            
-
           <div className="">
-          <span className="px-20 text-2xl text-center text-white ">
-              Порака до младите:{" "}
-              </span>
             <button
               type="submit"
-              className="w-2/4 bg-amber-500 border-2 text-white mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline"
+              className="mx-auto w-11/12 md:w-2/4 bg-amber-500 text-white mt-2 p-3 rounded-sm focus:outline-none focus:shadow-outline"
             >
               Send
             </button>
@@ -215,6 +211,9 @@ const Contact = () => {
 
 
 
+          <div>
+            <img src ={FooterFlower} alt="Logo" className="mx-auto md:-mt-20 md:pt-20 md:px-20" />
+          </div>
 
         </form>
       </div>
@@ -225,19 +224,18 @@ const Contact = () => {
       
       </div>
 
-   
-
-
-
-
-
-
-
-
+      <div className="bg-white max-h-96 sm:mt-20 mb-10 ">
+     <footer> Created with love by <span className="text-amber-500 ">Sanja & Ivan</span> 
+    <div>  2022 </div> </footer>
+   </div> 
 
   </div>
 
+
+  
+
     </section>
+    
 
     {/* <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
    <StaticImage src="../images/background.jpg" alt="gatsby astronaut" />
